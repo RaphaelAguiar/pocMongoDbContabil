f47a55a05103ae57eb717078c3834a7b98cce9d2
raphael.aguiar <carvalhodeaguiar123@gmail.com>
2018-03-17 11:42:22 -0300

    teste

---
+++ src/main/java/br/com/rca/poc/mongodb/Contabil/ContabilApplication.java
@@ -0,0 +1,12 @@
+package br.com.rca.poc.mongodb.Contabil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContabilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContabilApplication.class, args);
+	}
+}
